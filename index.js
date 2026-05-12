34084151eff3a00ef95a8c883aec7832303f33b3
sailaja-dev <psailaja90@gmail.com>
2026-05-12 20:09:37 +0530

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 function handleFormSubmit(event) {
     event.preventDefault();
-    let username = event.target.username.value;
-    let email = event.target.email.value;
-    let phone = event.target.phone.value;
-    localStorage.setItem("username", username);
-    localStorage.setItem("email", email);
-    localStorage.setItem("phone", phone);
+    const username = event.target.username.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value;
+    const obj = {
+        username, email, phone
+    }
+    const stingObj = JSON.stringify(obj);
+    localStorage.setItem(email, stingObj);
+    displayUser(obj);
+}
+function displayUser(obj) {
+
+
+
+    const ul = document.querySelector('ul');
     
+    const newli = document.createElement('li');
+    newli.textContent = obj.username + "-" + obj.email + "-" + obj.phone;
     
+
+    const newButton = document.createElement('button');
+
+    newButton.textContent = 'Delete';
+    newButton.addEventListener("click", () => {
+        newli.remove();
+        localStorage.removeItem(obj.email);
+    })
+    newli.appendChild(newButton);
+    ul.appendChild(newli);
+
+
 }
 module.exports = handleFormSubmit;
